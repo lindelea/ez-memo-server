a3fa3ad25328a91d8edd32528fd3c5c542503b9c
lindelela <kgo@lindelin.org>
2021-10-15 13:04:07 +0900

    モデル作成

---
+++ database/migrations/2021_10_15_034801_create_folders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoldersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->uuid('parent_id')->nullable();
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folders');
+    }
+}
