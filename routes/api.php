d8b973984dd0b50bef2f7f6799a4b0205d2d9c44
lindelela <kgo@lindelin.org>
2021-10-22 12:41:41 +0900

    ユーザ登録API 作成

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('App\Http\Controllers\API\V1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', 'UserController@register');
+    });
+});
